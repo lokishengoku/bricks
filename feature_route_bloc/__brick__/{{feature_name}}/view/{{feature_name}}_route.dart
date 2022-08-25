9fab76fd646e5adb9672c5b47fe3c57be0cdf47a
lokishengoku <dthuanle.20@gmail.com>
2022-08-25 11:33:57 +0700

    add named route, scaffold

--- feature_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_route.dart
+++ feature_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_route.dart
@@ -5,6 +5,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class {{feature_name.pascalCase()}}Route extends StatelessWidget {
   const {{feature_name.pascalCase()}}Route({Key? key}) : super(key: key);
 
+  static const String name = '${{feature_name}}';
+
   static Route<void> route() {
     return MaterialPageRoute(
       builder: (ctx) {
@@ -18,6 +20,9 @@ class {{feature_name.pascalCase()}}Route extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const {{feature_name.pascalCase()}}View();
+    return BlocProvider(
+      create: (_) => {{feature_name.pascalCase()}}Bloc(),
+      child: const {{feature_name.pascalCase()}}View(),
+    );
   }
 }
--- feature_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_view.dart
+++ feature_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_view.dart
@@ -7,20 +7,22 @@ class {{feature_name.pascalCase()}}View extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<{{feature_name.pascalCase()}}Bloc, {{feature_name.pascalCase()}}State>(
-      listenWhen: (previous, current) => current.status == {{feature_name.pascalCase()}}Status.error,
-      listener: (BuildContext context, {{feature_name.pascalCase()}}State state) {
-        if (state.status == {{feature_name.pascalCase()}}Status.error) {
-          //handel error
-        }
-      },
-      buildWhen: (previous, current) => current.status != previous.status,
-      builder: (BuildContext context, {{feature_name.pascalCase()}}State state) {
-        if(state.status == {{feature_name.pascalCase()}}Status.initial){
+    return Scaffold(
+      body: BlocConsumer<{{feature_name.pascalCase()}}Bloc, {{feature_name.pascalCase()}}State>(
+        listenWhen: (previous, current) => current.status == {{feature_name.pascalCase()}}Status.error,
+        listener: (BuildContext context, {{feature_name.pascalCase()}}State state) {
+          if (state.status == {{feature_name.pascalCase()}}Status.error) {
+            //handel error
+          }
+        },
+        buildWhen: (previous, current) => current.status != previous.status,
+        builder: (BuildContext context, {{feature_name.pascalCase()}}State state) {
+          if(state.status == {{feature_name.pascalCase()}}Status.initial){
+            return Container();
+          }
           return Container();
-        }
-        return Container();
-      },
+        },
+      ),
     );
   }
 }
